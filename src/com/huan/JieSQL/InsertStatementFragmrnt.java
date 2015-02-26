86da54a4b4502957476d360162619c197d4fc6ff
JHuan <408037640@qq.com>
2015-02-26 19:39:16 +0800

    Add jdbc
    Still some problems...It seems take some time to adapt to it...

--- src/com/huan/JieSQL/InsertStatementFragmrnt.java
+++ src/com/huan/JieSQL/InsertStatementFragmrnt.java
@@ -9,7 +9,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import com.huan.JieSQL.util.MySQLHelper;
+
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by barryjiang on 2015/2/15.
@@ -18,18 +25,56 @@ public class InsertStatementFragmrnt extends Fragment {
 
     View mRootView;
 
-    ActionBar mActionBar;
 
-    EditText mSQLResultText;
+    EditText        mSQLResultText;
+    Button          mCommitButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         mRootView = inflater.inflate(R.layout.fragment_sql_statment, container, false);
 
-        //Initialize editText.
         mSQLResultText = (EditText)mRootView.findViewById(R.id.editTextSQLResult);
+        mCommitButton = (Button)mRootView.findViewById(R.id.buttonCommit);
+
+        mCommitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testmySQL();
+            }
+        });
 
         return mRootView;
+    }
+
+    private void testmySQL(){
+
+        String url = "jdbc:mysql://192.168.102.128:8555/NodeJsSample";
+        String user = "root";
+        String pwd = "";
+
+        MySQLHelper sqlHelper = new MySQLHelper();
+        if(sqlHelper.connect2DB(url,user,pwd)){
+
+            //test query
+
+            List<Map<String,Object>> queryResult = null;
+            List<String> paraQuery = new LinkedList<String>();
+            paraQuery.add("Ted");
+            try{
+                queryResult = sqlHelper.getQueryResult("SELECT user_name,user_age FROM NodeJsSample.UserInfo where user_name = ?;",paraQuery);
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+
+            String result = "";
+            for(Map<String,Object> map : queryResult){
+                for(Map.Entry<String,Object> entry:map.entrySet()){
+                    result+=entry.getKey()+"\t"+entry.getValue()+"\n";
+                }
+            }
+
+            mSQLResultText.setText(result);
+        }
     }
 }
---
+++ src/com/huan/JieSQL/util/MySQLHelper.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2015.
+ * @author barryjiang's.
+ */
+
+package com.huan.JieSQL.util;
+
+import com.mysql.jdbc.*;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSetMetaData;
+import java.util.*;
+
+/**
+ * Well, as the same way, jdbc is the simplest way,so...
+ * Created by barryjiang on 2015/2/26.
+ */
+public class MySQLHelper {
+
+    private Connection mConnection;
+
+    public boolean connect2DB(String url,String userName, String password){
+
+        boolean connected = false;
+        try {
+            Class.forName("com.mysql.jdbc.Drive");
+            mConnection = DriverManager.getConnection(url, userName, password);
+            connected = true;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return connected;
+    }
+
+    //get sql query result. sql statement require the format of "select ? where name = ?"
+    public  List<Map<String,Object>> getQueryResult(String sql,List<String> para) throws SQLException{
+
+        List<Map<String, Object>> result = new LinkedList<Map<String, Object>>();
+
+        PreparedStatement statment = mConnection.prepareStatement(sql);
+
+        if(para!=null && para.size()>0){
+            for(int i=0;i<para.size();i++){
+                //NOTICE: sql statement slot seems it use 1 index rather than 0 index...
+                statment.setObject(i+1,para.get(i));
+            }
+        }
+
+        ResultSet resultSet = statment.executeQuery();
+        ResultSetMetaData resultSetMetaData = statment.getMetaData();
+
+        //get column's length
+        int columnLen = resultSetMetaData.getColumnCount();
+        if(resultSet.first()){
+            do{
+                Map<String,Object> map = new HashMap<String, Object>();
+                for(int i = 0; i<columnLen; i++){
+                    String columnName = resultSetMetaData.getColumnName(i+1);
+                    Object object = resultSet.getObject(i);
+                    map.put(columnName,object);
+                }
+                result.add(map);
+            }while(resultSet.next());
+        }
+        return result;
+
+    }
+
+    public int excuteUpdateSql(String sql,List<String> para) throws SQLException{
+
+        PreparedStatement statment = mConnection.prepareStatement(sql);
+
+        if(para!=null && para.size()>0){
+            for(int i=0;i<para.size();i++){
+                //NOTICE: sql statement slot seems it use 1 index rather than 0 index...
+                statment.setObject(i+1,para.get(i));
+            }
+        }
+
+        return statment.executeUpdate();
+    }
+
+
+}
